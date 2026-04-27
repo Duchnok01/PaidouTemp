68b4f6e98b8a5039b4dfbb391a5c729769b6d357
duchnok01 <cleok1000@gmail.com>
2026-04-27 12:35:56 +0200

    e

---
+++ paidou/src/main/java/fr/paidou/paidou/PaidouApplication.java
@@ -0,0 +1,13 @@
+package fr.paidou.paidou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaidouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaidouApplication.class, args);
+	}
+
+}
---
+++ paidou/src/test/java/fr/paidou/paidou/PaidouApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.paidou.paidou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaidouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
